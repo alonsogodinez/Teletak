379afec3e65effaa24be040c1a2aa8151429ddd3
Alonso <alonxogs@gmail.com>
2015-06-08 12:57:27 -0500

    Mixin de Mensaje de exito (alerta)  personalizado Eliminar trabajadores con modal de boostrap fixes en templates y css se añadio javascript de lado del cliente (vacio aun)

--- apps/usuarios/urls.py
+++ apps/usuarios/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns,  url
 
-from .views import Configuracion, RegistrarTrabajador,ListarTrabajadores,EditarTrabajador
+from .views import Configuracion, RegistrarTrabajador,ListarTrabajadores,EditarTrabajador,EliminarTrabajador
 
 urlpatterns = patterns('',
 
     url(r'^$', Configuracion.as_view(),name='index'),
     url(r'^nuevo/$', RegistrarTrabajador.as_view(),name='add_trabajador'),
-    url(r'^list/$', ListarTrabajadores.as_view(),name='trabajadores_list'),
-    url(r'^(?P<pk>\d+)/$', EditarTrabajador.as_view(),name='trabajador_detail'),
+    url(r'^lista/$', ListarTrabajadores.as_view(),name='trabajadores_list'),
+    url(r'^editar/(?P<pk>\d+)/$', EditarTrabajador.as_view(),name='trabajador_detail'),
+    url(r'^eliminar/(?P<pk>\d+)/$', EliminarTrabajador.as_view(),name='trabajador_delete'),
 
 )
