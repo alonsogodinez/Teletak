fe6a2dd8a93bd464e4cbf657d870dfee12e60d61
angel <aleangrodriguez123@gmail.com>
2015-06-17 20:45:23 -0500

    Haciendo formularios para añadir nuevos productos

--- apps/productos/forms.py
+++ apps/productos/forms.py
@@ -1,1 +1,16 @@
 __author__ = 'kevin'
+
+from django import forms
+from apps.almacen.models import Producto
+
+class RegistrarProductoForm(forms.ModelForm):
+    codigo = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class': 'form-control'}), label="Codigo")
+    sap = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control '}), label="SAP")
+    descripcion = forms.EmailField(max_length=30, widget=forms.EmailInput(attrs={'class': 'form-control'}),
+                                   label="Descripcion")
+    categoria = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control '}),
+                                label="Categoria")
+
+
+
+
