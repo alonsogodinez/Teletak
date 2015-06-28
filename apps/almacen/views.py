ee3fcc0ec88963bea06cc0b9948f3f3994ca4c3f
angel <aleangrodriguez321@hotmail.com>
2015-06-28 17:05:24 -0500

    Modificando vistas de almacen para el ingreso

--- apps/almacen/views.py
+++ apps/almacen/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.generic import View
 from forms import *
+from django.http import HttpResponse,HttpResponseRedirect
+from Teletak.mixins import SuccessMessageMixin
+
 class LoginRequiredMixin(object):
     u"""Ensures that user must be authenticated in order to access view."""
 
@@ -21,7 +24,7 @@ class Operaciones(LoginRequiredMixin,View):
     def get(self,request):
         return render(request,self.template_name)
 
-class Ingresos(LoginRequiredMixin,View):
+class Ingresos(LoginRequiredMixin,View,SuccessMessageMixin):
     template_name='almacen/ingresos/index.html'
     def get(self,request):
         return render(request,self.template_name)
@@ -32,8 +35,26 @@ class Ingresos(LoginRequiredMixin,View):
         producto_form = ProductoForm(request.POST)
         guiaremision_form = GuiaRemisionForm(request.POST)
         proveedores_form = ProveedoresForm(request.POST)
+        if usuario_form.is_valid() and ingresos_form.is_valid() and detalleingreso_form.is_valid() and producto_form.is_valid() and guiaremision_form.is_valid() and proveedores_form.is_valid():
+            nuevo_item = ingresos_form.save(commit=False)
+            nuevo_item.guia_remision = guiaremision_form.save()
+            nuevo_item.dni = usuario_form.save()
+            nuevo_item1 = detalleingreso_form.save(commit=False)
+            nuevo_item1.id_ingreso=nuevo_item.save()
+            nuevo_item1.codigo_producto=producto_form.save()
+            nuevo_item1.save()
+            success_message = 'Los datos se actualizaron correctamente'
+            return HttpResponseRedirect("/ingresos/")
+        else:
+            template_name = 'almacen/ingresos'
+            usuario_form = UsuarioForm
+            ingresos_form = IngresoForm
+            detalleingreso_form = DetalleIngreso
+            producto_form = ProductoForm
+            guiaremision_form = GuiaRemisionForm
+            proveedores_form = ProveedoresForm
 
-
+            return render(request,template_name,locals())
 
 class Reingresos(LoginRequiredMixin,View):
     template_name='almacen/reingresos/index.html'
