681102d8b7ee1cfe01a293514ec27051bc407a16
angel <aleangrodriguez321@hotmail.com>
2015-06-28 01:01:38 -0500

    Creado los formularios para la entrada y avanzando la vistas para los formularios

--- apps/almacen/views.py
+++ apps/almacen/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.generic import View
-
+from forms import *
 class LoginRequiredMixin(object):
     u"""Ensures that user must be authenticated in order to access view."""
 
@@ -25,6 +25,15 @@ class Ingresos(LoginRequiredMixin,View):
     template_name='almacen/ingresos/index.html'
     def get(self,request):
         return render(request,self.template_name)
+    def post(self,request):
+        usuario_form = UsuarioForm(request.POST)
+        ingresos_form= IngresoForm(request.POST)
+        detalleingreso_form = DetalleIngresoForm(request.POST)
+        producto_form = ProductoForm(request.POST)
+        guiaremision_form = GuiaRemisionForm(request.POST)
+        proveedores_form = ProveedoresForm(request.POST)
+
+
 
 class Reingresos(LoginRequiredMixin,View):
     template_name='almacen/reingresos/index.html'
@@ -35,3 +44,4 @@ class Salidas(LoginRequiredMixin,View):
     template_name='almacen/salidas/index.html'
     def get(self,request):
         return render(request,self.template_name)
+
