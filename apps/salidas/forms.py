82286709e4fc9e1184ba5215a5639d664b232d96
kevin <kevin_22894@hotmail.com>
2015-06-28 10:15:13 -0500

    sape

---
+++ apps/salidas/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class SalidaForm(forms.Modelform):
+    class Meta:
+        model = Salida
---
+++ apps/salidas/views.py
@@ -0,0 +1,3 @@
+from django.shortcuts import render
+from apps.almacen.models import *
+# Create your views here.
