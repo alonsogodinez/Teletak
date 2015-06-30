a80902391fbbef8f7973689ad206a317512be9b1
Alonso <alonxogs@gmail.com>
2015-06-30 16:01:20 -0500

    fix vista de registrar producto + template mejora template de lista producto

--- apps/productos/forms.py
+++ apps/productos/forms.py
@@ -1,12 +1,22 @@
+# coding=utf-8
 from django import forms
 from django.forms import ModelForm
 from .models import Producto,Categoria,UnidadMedicion,ProductoMedida
 
 class ProductoForm(forms.ModelForm):
-    sap = forms.CharField(max_length=10,widget=forms.TextInput(attrs={'class':'form-control'}),label="SAP")
-    descripcion = forms.CharField(max_length=50,widget=forms.TextInput(attrs={'class':'form-control'}),label="Descripcion")
-    categoria = forms.ModelChoiceField(queryset=Categoria.objects.all(),widget=forms.Select(attrs={'class':'form-control'}),label="Categoria")
-    stock_minimo = forms.IntegerField(widget=forms.NumberInput(attrs={'class':'form-control'}),label="Stock Minimo")
+    sap = forms.CharField(max_length=10,
+                          widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Código SAP'}),
+                          label="SAP")
+    descripcion = forms.CharField(max_length=50,
+                                  widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Descripción'}),
+                                  label="Descripcion")
+    categoria = forms.ModelChoiceField(queryset=Categoria.objects.all(),
+                                       widget=forms.Select(attrs={'class':'form-control','placeholder':'Categoría'}),
+                                       label="Categoria")
+    stock_minimo = forms.IntegerField(widget=forms.NumberInput(attrs={'class':'form-control',
+                                                                      'placeholder':'Stock mínimo'}),
+                                      label="Stock Minimo")
+
     class Meta:
         model = Producto
         fields = ('sap','descripcion','categoria','stock_minimo',)
