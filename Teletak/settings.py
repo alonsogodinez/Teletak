5463a539145e3729085d03d6409a40ba318abaa4
Alonso <alonxogs@gmail.com>
2015-07-13 22:15:19 -0500

    segundo commit

--- Teletak/settings.py
+++ Teletak/settings.py
@@ -18,7 +18,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
