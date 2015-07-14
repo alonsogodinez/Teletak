088492df44db822b716b47d8c04a295ecd29268b
Alonso <alonxogs@gmail.com>
2015-07-13 22:12:05 -0500

    segundo commit

--- Teletak/settings.py
+++ Teletak/settings.py
@@ -1,28 +1,26 @@
 
 # -*- encoding: utf-8 -*-
-
+import os
 from .settingswrap.base import *
-
+import dj_database_url
 
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'teletak2',
-        'USER': 'Alonso',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
-
+DATABASES['default'] =  dj_database_url.config()
+DATABASES['default']['ENGINE'] = 'django_postgrespool'
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = [BASE_DIR.child('static')]
+STATIC_ROOT = 'staticfiles'
 
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [BASE_DIR.child('static')]
 
