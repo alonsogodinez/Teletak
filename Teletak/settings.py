a0e8ff3b20444431e05c891aed07203848d0f504
Alonso <alonxogs@gmail.com>
2015-07-13 22:37:05 -0500

    segundo commit

--- Teletak/settings.py
+++ Teletak/settings.py
@@ -18,7 +18,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = [BASE_DIR.child('Teletak').child('static')]
+STATIC_ROOT = '/static/'
 
 STATICFILES_DIRS = [BASE_DIR.child('Teletak').child('static')]
 
