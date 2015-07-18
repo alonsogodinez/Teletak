319b14179258e76e357eae2215ba801036f636f6
Alonso <alonxogs@gmail.com>
2015-07-17 21:31:00 -0500

    heroku

--- Teletak/settings.py
+++ Teletak/settings.py
@@ -7,14 +7,14 @@ import os
 import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'teletak2',
-        'USER': 'Alonso',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'teletak2',
+    #     'USER': 'Alonso',
+    #     'PASSWORD': '123456',
+    #     'HOST': 'localhost',
+    #     'PORT': '',
+    # }
 }
 # DATABASES['default'] =  dj_database_url.config()
 # DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
