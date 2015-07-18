f3f70ae8586922971e2aece39d1c2baecc0a078b
Alonso <alonxogs@gmail.com>
2015-07-17 21:19:12 -0500

    heroku

--- Teletak/settings.py
+++ Teletak/settings.py
@@ -7,30 +7,31 @@ import os
 import dj_database_url
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'teletak2',
-    #     'USER': 'Alonso',
-    #     'PASSWORD': '123456',
-    #     'HOST': 'localhost',
-    #     'PORT': '',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'teletak2',
+        'USER': 'Alonso',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
 }
-DATABASES['default'] =  dj_database_url.config()
-DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+# DATABASES['default'] =  dj_database_url.config()
+# DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
 
 STATIC_URL = '/static/'
 
 
 
-STATIC_ROOT = 'static'
+STATIC_ROOT = '../static'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    'Teletak/static/css',
-    'Teletak/static/js',
-    'Teletak/static/fonts',
-    'Teletak/static/img'
+    os.path.join(BASE_DIR, 'static/css'),
+    os.path.join(BASE_DIR, 'static/js'),
+    os.path.join(BASE_DIR, 'static/fonts'),
+    os.path.join(BASE_DIR, 'static/img'),
+
 )
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
--- Teletak/wsgi.py
+++ Teletak/wsgi.py
@@ -16,4 +16,4 @@ from whitenoise.django import DjangoWhiteNoise
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Teletak.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+# application = DjangoWhiteNoise(application)
