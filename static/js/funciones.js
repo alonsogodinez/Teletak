6f088aa105c38c0d2f1238151b195fd163ff3355
kevin <kevin_22894@hotmail.com>
2015-07-18 22:07:04 -0500

    reporte

--- static/js/funciones.js
+++ static/js/funciones.js
@@ -200,6 +200,49 @@ $(document).ready(function() {
 
     $('#datatable-responsive').DataTable(datatableresponsive);
 
+    $('#reporte').DataTable( {
+       "dom": 'T<"clear">lfrtip',
+        tableTools: {
+            "sSwfPath": "../swf/copy_csv_xls_pdf.swf"
+        },
+        "oTableTools": {
+            "aButtons": [
+                {
+                    "sExtends": "ajax",
+                    "bFooter": fal
+                }
+            ]
+        },
+       "oLanguage": {
+            "oAria": {
+                "sSortAscending": " - ordenar de forma ascendente",
+                "sSortDescending": " - ordenar de forma descendente",
+                "sInfoEmpty": "No hay información para mostrar",
+                "sLengthMenu": "Mostrar _MENU_ registros",
+                "sSearch": "Buscar :",
+                "sZeroRecords": "No hay ningún registro",
+
+            },
+            "oPaginate": {
+                "sFirst": "Primera página",
+                "sLast": "Última página",
+                "sNext": "Página siguiente",
+                "sPrevious": "Página anterior"
+            },
+
+            "sEmptyTable": "Esta tabla no tiene datos",
+            "sInfo": "Mostrando  _START_ - _END_ de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrando 0 entradas",
+            "sInfoFiltered": "(filtrados de _MAX_  registros en total)",
+            "sLengthMenu": "Motrar _MENU_ registros",
+            "sSearch": "Buscar :",
+            "sZeroRecords": "No se encontraron coincidencias"
+
+        }
+
+    } );
+
+
 });
 
 
