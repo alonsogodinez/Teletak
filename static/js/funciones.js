76bf36408badb507ad4c3155e0b3424de46cef4e
Alonso <alonxogs@gmail.com>
2015-07-18 23:12:29 -0500

    heroku

--- static/js/funciones.js
+++ static/js/funciones.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
     });
 
     $(".chosen-select").chosen({no_results_text: "No se encontró coincidencias con :"});
-
+ //
     $('#detalle').modal('show');
 
     $('#id_devolucion').change(function () {
